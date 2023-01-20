3144166be37230c7392f8eb976f98aef9167e372
Marcos Ivan Robles Hernandez <marcos.robles@dalton.com.mx>
2023-01-20 17:46:39 -0600

    Filtering and Arrow functions

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -23,8 +23,9 @@ export class ProductListComponent implements OnInit{
    {
        this._listFilter= value;
        console.log('In setter:',value);
+       this.filteredProducts = this.performFilter(value);
    }
-
+filteredProducts:IProduct[] = [];
     products: IProduct[] = [
         {
           "productId": 2,
@@ -48,6 +49,12 @@ export class ProductListComponent implements OnInit{
         }
       ];
 
+      performFilter(filterBy:string):IProduct[]{
+
+        filterBy = filterBy.toLocaleLowerCase();
+        return this.products.filter((product:IProduct)=>
+        product.productName.toLocaleLowerCase().includes(filterBy));
+      }
       toggleImage(): void{
           this.showImage = !this.showImage;
       }
