c7f5e30c27c30f044fbfeca5ddccc978980fd494
Marcos Ivan Robles Hernandez <marcos.robles@dalton.com.mx>
2023-01-26 22:15:16 -0600

    Retrieving Data Using Http, Observable

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
@@ -15,7 +17,8 @@ import { StartComponent } from './shared/start.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   bootstrap: [AppComponent]
 })
--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Subscription } from "rxjs";
 import { IProduct } from "./product";
 import { ProductService } from "./product.service";
 
@@ -7,13 +8,16 @@ import { ProductService } from "./product.service";
     templateUrl:'./product-list.component.html',
     styleUrls:['./product-list.component.css']
 })
-export class ProductListComponent implements OnInit{
+export class ProductListComponent implements OnInit,OnDestroy{
    
   constructor(private productService:ProductService){}
     pageTitle = 'Product List';
     imageWidth:number =50; //You cand add or not the type. Angular infer the type
     imageMargin = 2;
     showImage: boolean = false;
+    errorMesagge:string='';
+    sub!:Subscription;
+    
    private _listFilter: string = '';
 
    get listFilter(): string{
@@ -40,10 +44,25 @@ filteredProducts:IProduct[] = [];
           this.showImage = !this.showImage;
       }
       ngOnInit(): void {
-        this.products = this.productService.getProducts();
-        this.filteredProducts = this.products; 
+        this.sub= this.productService.getProducts().subscribe(
+          {
+            next: products=>
+            {
+              this.products = products;
+              this.filteredProducts = this.products; 
+            } ,
+            
+      
+            error: err=> this.errorMesagge = err
+          }
+        );
+      
     }
 
+    ngOnDestroy():void{
+this.sub.unsubscribe();
+
+    }
     onRatingClicked(message:string):void{
       this.pageTitle = 'Product List: '+ message;
     }
